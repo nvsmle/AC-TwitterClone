759e5e6dcfb281ba0f9909d30fb794c2d510231d
nvsmle <alexandr-super@mail.ru>
2020-05-21 13:34:19 +0700

    set up a parse server

--- app/src/main/java/neversmile/packag/com/ac_twitterclone/MainActivity.java
+++ app/src/main/java/neversmile/packag/com/ac_twitterclone/MainActivity.java
@@ -1,8 +1,10 @@
 package neversmile.packag.com.ac_twitterclone;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.parse.ParseInstallation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
     }
 }
