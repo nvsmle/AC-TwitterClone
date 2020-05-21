3bf9d12c0f7dbeb721944fb3ca411a80fd6fe77f
nvsmle <alexandr-super@mail.ru>
2020-05-21 12:50:47 +0700

    Initial commit

---
+++ app/src/main/java/neversmile/packag/com/ac_twitterclone/MainActivity.java
@@ -0,0 +1,14 @@
+package neversmile.packag.com.ac_twitterclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
